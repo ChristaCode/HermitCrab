758f03f62b2f46e3a3f463743ab5b3e1c723423f
Chel <c.l.webster@live.com>
2019-01-25 21:43:21 +0000

    Chel's scene added

---
+++ Assets/CamFollowPlayer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollowPlayer : MonoBehaviour {
+
+    [SerializeField] float FollowSpeed = 5f;
+    [SerializeField] float ZoomSpeed = 1f;
+    [SerializeField] float OffsetX = 0f;
+    [SerializeField] float OffsetY = 10f;
+    [SerializeField] float OffsetZ = 10f;
+
+    [SerializeField] float ZoomDelay = 2f;
+    float zoomOutDelayCounter;
+    float zoomInDelayCounter;
+    float camMinDist = 40f;
+    float camMaxDist = 60f;
+    public float CamSpeed;
+    bool isMovingUp;
+    float lastPos;
+  
+
+    Transform _playerPos;
+    
+    void Start()
+    {
+        lastPos = transform.position.y;
+        _playerPos = GameObject.Find("Cam Target").transform;
+
+       
+
+    }
+    void Update ()
+    {     
+        CamSpeed = (transform.position.y - lastPos) / Time.deltaTime;
+
+        if (transform.position.y >= lastPos)
+            isMovingUp = true;
+        else
+            isMovingUp = false;
+            
+        lastPos = transform.position.y;
+
+        var targetPos = new Vector3(_playerPos.position.x + OffsetX, _playerPos.position.y + OffsetY, _playerPos.position.z + OffsetZ);
+        transform.position -= (transform.position - targetPos) * FollowSpeed * Time.deltaTime;
+
+        var camSize = Camera.main.fieldOfView;
+
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            if(zoomInDelayCounter != 0)
+                zoomInDelayCounter = 0f;
+
+            zoomOutDelayCounter += Time.deltaTime;
+            
+            if (camSize < camMaxDist && zoomOutDelayCounter > ZoomDelay/3)
+                Camera.main.fieldOfView = Mathf.Lerp(camSize, camMaxDist, ZoomSpeed * Time.deltaTime);
+        }
+        else
+        {
+            if (zoomOutDelayCounter != 0)
+                zoomOutDelayCounter = 0f;
+
+                zoomInDelayCounter += Time.deltaTime;
+
+            if (camSize > camMinDist && zoomInDelayCounter > ZoomDelay)
+                Camera.main.fieldOfView = Mathf.Lerp(camSize, camMinDist, ZoomSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public static Player Instance;
+    public float MaxSpeed = 10f;
+    [SerializeField] float ScareDuration = 1f;
+    float MoveHorizontal;
+    float MoveVertical;
+    bool facingRight;
+    Vector3 targetPos;
+    SpriteRenderer doggoSprite;
+    Animator doggoAnimations;
+    AudioSource bark;
+    float idleTimer;
+    [SerializeField] AudioClip[] Barks;
+    [SerializeField] GameObject _woofLines;
+    Animator _woofAnim;
+    SpriteRenderer _woofSprite;
+
+    public float CurrentSpeed;
+
+    Rigidbody _rb;
+
+    SpriteRenderer _mySprite;
+
+    void Start ()
+    {
+        Instance = this;
+        doggoSprite = GetComponent<SpriteRenderer>();
+        doggoAnimations = GetComponent<Animator>();
+        bark = GetComponent<AudioSource>();
+        _rb = GetComponent<Rigidbody>();
+        //_mySprite = GetComponent<SpriteRenderer>();
+
+        _woofAnim = _woofLines.GetComponent<Animator>();
+        _woofSprite = _woofLines.GetComponent<SpriteRenderer>();
+
+    }
+    
+    void Update()
+    {
+        //if (SceneMgr.Instance.GameOver)
+          //  return;
+
+        //_mySprite.sortingOrder = Mathf.RoundToInt(transform.position.y * 100f) * -1; //this means the dog will be drawn in fron of or behind items if it is higher/lower than them
+
+        if (Input.GetButtonDown("Bark"))
+        {
+            if (bark.isPlaying)
+                return;
+
+            int b = Random.Range(0, 3);
+            bark.clip = Barks[b];
+            _woofAnim.SetTrigger("isBarking");
+
+            bark.Play();
+            StartCoroutine("ScareSheep");
+        }
+
+        if (facingRight)
+        {
+            if (!doggoSprite.flipX)
+            {
+                doggoSprite.flipX = true;
+                _woofSprite.flipX = true;
+
+                _woofLines.transform.localPosition = new Vector2(4.41f, -0.28f);
+            }
+        }
+        else
+        {
+            if (doggoSprite.flipX)
+            {
+                doggoSprite.flipX = false;
+                _woofSprite.flipX = false;
+
+                _woofLines.transform.localPosition = new Vector2(-4.41f, -0.28f);
+            }
+        }
+
+        if (MoveHorizontal > 0 && !facingRight)
+            facingRight = true;
+        else if (MoveHorizontal < 0 && facingRight)
+            facingRight = false;
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
+            {
+                // move the object smoothly to the touch position
+                Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
+                transform.position = Vector3.Lerp(transform.position, touchPosition, Time.deltaTime);
+                //Vector3 tarDirection = transform.position - touchPosition;
+                //print(tarDirection);
+                //_rb.velocity = new Vector2(tarDirection.x * MaxSpeed, tarDirection.y * MaxSpeed);
+
+                MoveHorizontal = 1;
+                MoveVertical = 1;
+
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        //if (SceneMgr.Instance.GameOver || SceneMgr.Instance.DisableMovement)
+        //{
+        //    GetComponent<Rigidbody2D>().Sleep();
+        //    MoveHorizontal = 0f;
+        //    MoveVertical = 0f;
+        //    return;
+        //}
+
+        //for Keyboard Input/////////////////////////////////////////////////////////////////////////////////////////////
+        MoveHorizontal = Input.GetAxis("Horizontal");
+        MoveVertical = Input.GetAxis("Vertical");
+
+        _rb.velocity = new Vector3(MoveHorizontal * MaxSpeed, _rb.velocity.y, MoveVertical * MaxSpeed);
+        CurrentSpeed = MoveVertical * MaxSpeed;
+
+        if (MoveHorizontal < -0.1 || MoveHorizontal > 0.1)
+            doggoAnimations.SetFloat("Speed", 1);
+        
+        if (MoveVertical < -0.1 || MoveVertical > 0.1)
+            doggoAnimations.SetFloat("Speed", 1);
+
+        if (MoveVertical == 0 && MoveHorizontal == 0)
+        {
+            doggoAnimations.SetFloat("Speed", 0);
+            idleTimer += Time.deltaTime;
+        }
+        else
+            if (idleTimer > 0)
+            idleTimer = 0f;
+
+        if (idleTimer >= 5)
+            doggoAnimations.SetTrigger("Sit");
+
+    }    
+}
