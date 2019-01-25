8ad8d84538e3ada74e0d721929bdbb3f5e120c79
Chel <c.l.webster@live.com>
2019-01-25 22:40:01 +0000

    here

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,25 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour {
 
     public static Player Instance;
     public float MaxSpeed = 10f;
-    [SerializeField] float ScareDuration = 1f;
     float MoveHorizontal;
     float MoveVertical;
     bool facingRight;
     Vector3 targetPos;
-    SpriteRenderer doggoSprite;
-    Animator doggoAnimations;
+    SpriteRenderer crabSprite;
+    Animator crabAnimations;
     AudioSource bark;
     float idleTimer;
-    [SerializeField] AudioClip[] Barks;
+    [SerializeField] AudioClip Barks;
     [SerializeField] GameObject _woofLines;
-    Animator _woofAnim;
-    SpriteRenderer _woofSprite;
 
+
+    public float PlayerSize = 1f;
     public float CurrentSpeed;
 
     Rigidbody _rb;
@@ -29,14 +26,13 @@ public class Player : MonoBehaviour {
     void Start ()
     {
         Instance = this;
-        doggoSprite = GetComponent<SpriteRenderer>();
-        doggoAnimations = GetComponent<Animator>();
+        crabSprite = GetComponent<SpriteRenderer>();
+        crabAnimations = GetComponent<Animator>();
         bark = GetComponent<AudioSource>();
         _rb = GetComponent<Rigidbody>();
         //_mySprite = GetComponent<SpriteRenderer>();
 
-        _woofAnim = _woofLines.GetComponent<Animator>();
-        _woofSprite = _woofLines.GetComponent<SpriteRenderer>();
+        
 
     }
     
@@ -47,7 +43,7 @@ public class Player : MonoBehaviour {
 
         //_mySprite.sortingOrder = Mathf.RoundToInt(transform.position.y * 100f) * -1; //this means the dog will be drawn in fron of or behind items if it is higher/lower than them
 
-        if (Input.GetButtonDown("Bark"))
+        /*if (Input.GetButtonDown("Bark"))
         {
             if (bark.isPlaying)
                 return;
@@ -58,52 +54,26 @@ public class Player : MonoBehaviour {
 
             bark.Play();
             StartCoroutine("ScareSheep");
-        }
+        }*/
 
         if (facingRight)
         {
-            if (!doggoSprite.flipX)
-            {
-                doggoSprite.flipX = true;
-                _woofSprite.flipX = true;
+            if (!crabSprite.flipX)            
+                crabSprite.flipX = true; 
 
-                _woofLines.transform.localPosition = new Vector2(4.41f, -0.28f);
-            }
         }
         else
         {
-            if (doggoSprite.flipX)
-            {
-                doggoSprite.flipX = false;
-                _woofSprite.flipX = false;
-
-                _woofLines.transform.localPosition = new Vector2(-4.41f, -0.28f);
-            }
+            if (crabSprite.flipX)            
+                crabSprite.flipX = false;              
+            
         }
 
         if (MoveHorizontal > 0 && !facingRight)
             facingRight = true;
         else if (MoveHorizontal < 0 && facingRight)
             facingRight = false;
-
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-
-            if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
-            {
-                // move the object smoothly to the touch position
-                Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
-                transform.position = Vector3.Lerp(transform.position, touchPosition, Time.deltaTime);
-                //Vector3 tarDirection = transform.position - touchPosition;
-                //print(tarDirection);
-                //_rb.velocity = new Vector2(tarDirection.x * MaxSpeed, tarDirection.y * MaxSpeed);
-
-                MoveHorizontal = 1;
-                MoveVertical = 1;
-
-            }
-        }
+        
     }
 
     void FixedUpdate()
@@ -125,14 +95,14 @@ public class Player : MonoBehaviour {
         CurrentSpeed = MoveVertical * MaxSpeed;
 
         if (MoveHorizontal < -0.1 || MoveHorizontal > 0.1)
-            doggoAnimations.SetFloat("Speed", 1);
+            crabAnimations.SetFloat("Speed", 1);
         
         if (MoveVertical < -0.1 || MoveVertical > 0.1)
-            doggoAnimations.SetFloat("Speed", 1);
+            crabAnimations.SetFloat("Speed", 1);
 
         if (MoveVertical == 0 && MoveHorizontal == 0)
         {
-            doggoAnimations.SetFloat("Speed", 0);
+            crabAnimations.SetFloat("Speed", 0);
             idleTimer += Time.deltaTime;
         }
         else
@@ -140,7 +110,7 @@ public class Player : MonoBehaviour {
             idleTimer = 0f;
 
         if (idleTimer >= 5)
-            doggoAnimations.SetTrigger("Sit");
+            crabAnimations.SetTrigger("Sit");
 
     }    
 }
--- Assets/food.cs
+++ Assets/food.cs
@@ -1,34 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class food : MonoBehaviour
 {
     Animator anim;                              // Reference to the animator component.
-    GameObject player;                          // Reference to the player GameObject.
+    //GameObject player;                          // Reference to the player GameObject.
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
         anim = GetComponent<Animator>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == player)
+        if (other.gameObject.tag == "Player")
         {
-            player.playerSize.currentSize += 1;
+            Player.Instance.PlayerSize += 1;
             anim.SetTrigger("PlayerGrowth");
-        }
-    }
 
-    void OnTriggerExit(Collider other)
-    {
-        // If the exiting collider is the player...
-        if (other.gameObject == player)
-        {
             Destroy(gameObject);
+
         }
     }
 }
