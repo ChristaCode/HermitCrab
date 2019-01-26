b6e5f7a7846ac17de66683d42207b441e5961be4
Chel <c.l.webster@live.com>
2019-01-26 11:48:12 +0000

    :)

---
+++ Assets/Scripts/food.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class food : MonoBehaviour
+{
+    Animator anim;                              // Reference to the animator component.
+    //GameObject player;                          // Reference to the player GameObject.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Player.Instance.PlayerSize += 1;
+            anim.SetTrigger("PlayerGrowth");
+
+            Destroy(gameObject);
+
+        }
+    }
+}
+
