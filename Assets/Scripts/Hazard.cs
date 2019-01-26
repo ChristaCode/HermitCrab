c2dfd967a2d3889e766f186a6fe00d5403d8d263
Kristin Murray <kemurray@whatsapp.com>
2019-01-26 14:45:22 +0000

    Hazard start

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Hazard : MonoBehaviour
 {
+    public float damage = .1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,40 @@ public class Hazard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Hazard/onTriggerEnter");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Player.Instance.CurrentHealth >= 0)
+            {
+                // casting to int here not a good idea? Player health should be
+                // defined as a float or we need to depleat it slower than 1 hp every
+                // update loop
+                Player.Instance.CurrentHealth -= (int)(damage * Time.deltaTime);
+            }
+            else
+            {
+                //kill player
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Hazard/onTriggerExit");
+        }
     }
 }
